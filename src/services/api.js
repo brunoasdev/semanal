0d4c978d443ee57c2d800b3ff555b32b6daef928
ieaditatiaia <31519813+ieaditatiaia@users.noreply.github.com>
2023-04-17 01:43:14 -0300

    alteração da baseUrl com https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://142.4.202.122:8080/events"
+    baseURL: "https://agenda.aditatiaia.com.br:8080/events"
 });
 
 export default api;
